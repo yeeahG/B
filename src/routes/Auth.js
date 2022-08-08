c94afbd4db131a541e71a5ea367bd9be0e1d7e9e
yeeahG <yeji.sep.k@gmail.com>
2022-08-09 01:26:28 +0900

    Auth setting, function 작성

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (event) => {
+    const {target: {name, value}} = event;
+
+    if(name === "email") {
+      setEmail(value);
+    } else if(name === "password") {
+      setPassword(value);
+    }
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <div>Auth</div>
+    <div>
+      <form onSubmit={onSubmit} >
+        <input 
+          required 
+          name="email"
+          type="text" 
+          placeholder="Email" 
+          value={email} 
+          onChange={onChange}
+        />
+        <input 
+          required 
+          name="password"
+          type="password" 
+          placeholder="Password" 
+          value={password} 
+          onChange={onChange}
+        />
+        <input type="submit" value="Log in" />
+      </form>
+      <div>
+        <button>Continue with GOOGLE</button>
+      </div>
+    </div>
   )
 }
 
