714a74026ad4fa21fafc2104fd04520f1c00acb1
yeeahG <yeji.sep.k@gmail.com>
2022-08-10 19:19:31 +0900

    Login, Login state 유지 기능

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "myFirebase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    authService.onAuthStateChanged( (user) => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  })
+
+  
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? 
+      <AppRouter isLoggedIn={isLoggedIn} /> 
+      : 
+      "Loading..." 
+      }
       <footer>&copy; B {new Date().getFullYear()}</footer>
     </>
   );
