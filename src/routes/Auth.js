f6b79cb9bccde6e6b4f5c57d83c119c99dca7aff
yeeahG <yeji.sep.k@gmail.com>
2022-08-10 17:15:44 +0900

    Using Firebase API, create Signin

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
+import { authService } from 'myFirebase';
 import React, { useState } from 'react'
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
 
   const onChange = (event) => {
     const {target: {name, value}} = event;
@@ -14,8 +16,22 @@ const Auth = () => {
     }
   }
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      let data;
+      if (newAccount) {
+        //create account
+        data = await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        //log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch(error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -37,7 +53,7 @@ const Auth = () => {
           value={password} 
           onChange={onChange}
         />
-        <input type="submit" value="Log in" />
+        <input type="submit" value={newAccount ? "Create Account" : "Log in"} />
       </form>
       <div>
         <button>Google</button>
