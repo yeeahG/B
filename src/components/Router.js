cb94d5f26b33b119217d5143936a8f53291956c9
yeeahG <yeji.sep.k@gmail.com>
2022-08-31 02:04:09 +0900

    Profile page에서 내가 쓴 Be 들만 보기

--- src/components/Router.js
+++ src/components/Router.js
@@ -15,7 +15,7 @@ const AppRouter = ( {isLoggedIn, userObj} ) => {
             {isLoggedIn ?
             <>
               <Route path="/" element={<Home userObj={userObj} />} />
-              <Route path="/profile" element={<Profile />} />
+              <Route path="/profile" element={<Profile userObj={userObj} />} />
             </>
             :
             <>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,27 @@
-import { authService } from 'myFirebase'
-import React from 'react'
+import { authService, dbService } from 'myFirebase'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const navigate = useNavigate();
   
   const onLogOutClick = () => {
     authService.signOut();
     navigate("/");
   }
+
+  const getMyBees = async() => {
+    const bees = await dbService
+      .collection("Bees")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt", "asc")
+      .get();
+      console.log(bees.docs.map((it) => it._delegate._document.data.value.mapValue.fields));
+  }
+
+  useEffect(() => {
+    getMyBees();
+  },[])
 
   return (
     <>
