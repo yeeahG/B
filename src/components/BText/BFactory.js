bc2df8737e7492254a5037c21c15d33e69301609
yeeahG <yeji.sep.k@gmail.com>
2022-09-05 23:42:29 +0900

    코드 정리

---
+++ src/components/BText/BFactory.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
+import { dbService, storageService } from 'myFirebase';
+import { v4 as uuidv4 } from 'uuid';
+
+const BFactory = ({ userObj }) => {
+    const [b, setB] = useState("");
+    const [fileAttach, setFileAttach] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        let fileUrl = "";
+    
+        if (fileAttach != "") {
+          const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+          const response = await uploadString(fileRef, fileAttach, "data_url");
+          fileUrl = await getDownloadURL(response.ref);
+        }
+        
+        const bObj = {
+          text: b,
+          createdAt: Date.now(),
+          creatorId: userObj.uid,
+          fileUrl
+        }
+        
+        await dbService.collection("Bees").add(bObj);
+        setB("");
+        setFileAttach("");
+      }
+    
+      const onChange = (event) => {
+        const { target : {value}} = event;
+        setB(value);
+      }
+    
+    
+      const onFileChange = (event) => {
+        // console.log(event.target.files);
+        const {
+          target: {files},
+        } = event;
+        const file = files[0];
+    
+        const fileReader = new FileReader();
+        fileReader.onloadend = (finishedEvent) => {
+          const { currentTarget: {result} } = finishedEvent;
+          setFileAttach(result);
+        }
+        fileReader.readAsDataURL(file);
+      }
+    
+      const onClearFile = () => {
+        setFileAttach("");
+      }
+
+  return (
+    <div>
+        
+        <form onSubmit={onSubmit}>
+        <input 
+          value={b}
+          onChange={onChange}
+          type="text" 
+          placeholder="What's on ur mind?" 
+          maxLength={120} 
+        />
+        <input type="file" accept="image/*" onChange={onFileChange} />
+        <input type="submit"  value="Be" />
+        {fileAttach && 
+        <div>
+          <img src={fileAttach} width="50px" height="50px" /> 
+          <button onClick={onClearFile}>X</button>
+        </div>
+        }
+      </form>
+      
+    </div>
+  )
+}
+
+export default BFactory
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,10 @@
-import BText from 'components/BText';
 import React, { useEffect, useState } from 'react'
-import { dbService, storageService } from 'myFirebase';
-import { v4 as uuidv4 } from 'uuid';
-import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import BText from 'components/BText/BText';
+import { dbService } from 'myFirebase';
+import BFactory from 'components/BText/BFactory';
 
 const Home = ( {userObj} ) => {
-  const [b, setB] = useState("");
   const [bs, setBs] = useState([]);
-  const [fileAttach, setFileAttach] = useState("");
 
   useEffect(() => {
     dbService.collection("Bees").onSnapshot(snapshot => {
@@ -19,72 +16,12 @@ const Home = ( {userObj} ) => {
     })
   }, [])
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let fileUrl = "";
-
-    if (fileAttach != "") {
-      const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
-      const response = await uploadString(fileRef, fileAttach, "data_url");
-      fileUrl = await getDownloadURL(response.ref);
-    }
-    
-    const bObj = {
-      text: b,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      fileUrl
-    }
-    
-    await dbService.collection("Bees").add(bObj);
-    setB("");
-    setFileAttach("");
-  }
-
-  const onChange = (event) => {
-    const { target : {value}} = event;
-    setB(value);
-  }
-
-
-  const onFileChange = (event) => {
-    // console.log(event.target.files);
-    const {
-      target: {files},
-    } = event;
-    const file = files[0];
-
-    const fileReader = new FileReader();
-    fileReader.onloadend = (finishedEvent) => {
-      const { currentTarget: {result} } = finishedEvent;
-      setFileAttach(result);
-    }
-    fileReader.readAsDataURL(file);
-  }
-
-  const onClearFile = () => {
-    setFileAttach("");
-  }
 
   return (
     <div className='container'>
-      <form onSubmit={onSubmit}>
-        <input 
-          value={b}
-          onChange={onChange}
-          type="text" 
-          placeholder="What's on ur mind?" 
-          maxLength={120} 
-        />
-        <input type="file" accept="image/*" onChange={onFileChange} />
-        <input type="submit"  value="Be" />
-        {fileAttach && 
-        <div>
-          <img src={fileAttach} width="50px" height="50px" /> 
-          <button onClick={onClearFile}>X</button>
-        </div>
-        }
-      </form>
+      <BFactory 
+        userObj={userObj}
+      />
 
       <div>
         {bs.map( it => 
